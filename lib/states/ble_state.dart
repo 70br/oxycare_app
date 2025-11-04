745f6ad8199a51386af442321f5daac008ed121f
brunosana <brunosanadev@gmail.com>
2025-11-03 21:36:06 -0300

    feat: create bluetooth state

---
+++ lib/states/ble_state.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:oxycare_app/utils.dart';
+
+class BleState extends ChangeNotifier {
+  bool connected = false;
+  BluetoothDevice? device;
+  List<ScanResult> lastResults = [];
+
+  StreamSubscription<List<int>>? _subscriptionCardio;
+  StreamSubscription<List<int>>? _subscriptionTemperatura;
+  StreamSubscription<List<int>>? _subscriptionRespiratoria;
+
+  double freqRespiratoria = 0;
+  int freqCardiaca = 0;
+  double temperatura = 0;
+  DateTime? lastUpdateTimestamp = null;
+
+  final Guid uuidServico = Guid(HardwareCharacteristics.service);
+  final Guid uuidFreqCardiaca = Guid(HardwareCharacteristics.heartRate);
+  final Guid uuidFreqRespiratoria = Guid(HardwareCharacteristics.respiratoryRate);
+  final Guid uuidTemperatura = Guid(HardwareCharacteristics.temperature);
+
+  Future setDisconnected() async {
+    if(device != null) {
+      await _subscriptionCardio?.cancel();
+      await _subscriptionTemperatura?.cancel();
+      await _subscriptionRespiratoria?.cancel();
+
+      await Future.delayed(const Duration(milliseconds: 400));
+      await device?.disconnect();
+      connected = false;
+      device = null;
+      notifyListeners();
+    }
+  }
+
+  Future setConnected(BluetoothDevice connectedDevice) async {
+    connected = true;
+    device = connectedDevice;
+    await _watchServicos(connectedDevice);
+    notifyListeners();
+  }
+
+  void updateScanResults(List<ScanResult> results) {
+    lastResults = results;
+    notifyListeners();
+  }
+
+
+  String _decode(List<int> value) {
+    return utf8.decode(value);
+  }
+
+  Future _watchServicos(BluetoothDevice device) async {
+    List<BluetoothService> services = await device.discoverServices();
+    for (BluetoothService s in services) {
+      if (s.uuid == uuidServico) {
+        for (BluetoothCharacteristic c in s.characteristics) {
+          if (c.uuid == uuidFreqCardiaca) {
+              await c.setNotifyValue(true);
+              _subscriptionCardio = c.onValueReceived.listen((value) {
+                freqCardiaca = int.parse(_decode(value));
+                notifyListeners();
+              });
+          }
+          if (c.uuid == uuidFreqRespiratoria) {
+              await c.setNotifyValue(true);
+              _subscriptionRespiratoria = c.onValueReceived.listen((value) {
+                freqRespiratoria = double.parse(_decode(value));
+                notifyListeners();
+            });
+          }
+          if (c.uuid == uuidTemperatura) {
+              await c.setNotifyValue(true);
+              _subscriptionRespiratoria = c.onValueReceived.listen((value) {
+                temperatura = double.parse(_decode(value));
+                lastUpdateTimestamp = DateTime.now();
+                notifyListeners();
+              });
+          }
+        }
+      }
+    }
+  }
+
+}
---
+++ lib/utils.dart
@@ -0,0 +1,8 @@
+final urlGlobal = "https://1e9899470783.ngrok-free.app";
+
+class HardwareCharacteristics {
+  static const service = "91bad492-b950-4226-aa2b-4ede9fa42f59";
+  static const respiratoryRate = "d1d25ab4-f91c-4ad4-b8b9-d2909b0cf8b5";
+  static const heartRate = "38344511-79a2-47da-9814-e13e4d943fe2";
+  static const temperature = "a423b1a5-ca20-4d5f-b6d7-110419c4aba3";
+}
